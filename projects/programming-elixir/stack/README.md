44b98e4816965be33f06c955d284fe11c3cdbaa4
Daniel Lauzon <daniel.lauzon@gmail.com>
2023-11-08 15:30:17 -0500

    stack: added push (cast)

--- projects/programming-elixir/stack/README.md
+++ projects/programming-elixir/stack/README.md
@@ -9,4 +9,5 @@ $ iex -S mix
 
 {:ok, pid} = GenServer.start_link(Stack.Server,["cat",3,2,1])
 GenServer.call(pid, :pop)
+GenServer.cast(pid, {:push, 42})
 ```
--- projects/programming-elixir/stack/lib/stack/server.ex
+++ projects/programming-elixir/stack/lib/stack/server.ex
@@ -11,4 +11,8 @@ defmodule Stack.Server do
   def handle_call(:pop, _from, [head | tail]) do
     {:reply, head, tail}
   end
+
+  def handle_cast({:push, item}, state) do
+    {:noreply, [item | state]}
+  end
 end
